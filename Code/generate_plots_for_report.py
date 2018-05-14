a9f0ab05e939570b3b63c2fe3b421df6f2b87563
Cathy Chen <cathychen2357@gmail.com>
2018-05-14 11:22:48 -0400

    Update tests and plot generation scripts.

--- Code/generate_plots_for_report.py
+++ Code/generate_plots_for_report.py
@@ -3,19 +3,33 @@ import numpy as np
 
 savepath = "../Figures/"
 n = 30
-d = 10
-k = 2
+d = 14
+k = 3
 r = 5
 layout={i:np.random.rand(2) for i in range(n)}
 
-print("Creating neuroidal model for n, d, k, r = %d, %d, %d, %d" % (n, d, k, r))
+print("*****CREATING VISUALIZATIONS FOR MODEL DESCRIPTION*****")
 net = neuroidal_model.NeuroidalNet(n,d,k,r)
 net.visualize_network(layout=layout, savename=savepath+"initialmodel")
 net.create_item("A", disjoint=True)
 net.visualize_network(layout=layout, savename=savepath+"itemrepresentation")
 
+# n = 10
+# neuron = 0
+# p = 0.3
+# a = np.random.choice([0.0, 1.0], (n,n), p=[1-p, p])
+# np.fill_diagonal(a, 0)
+# G = nx.Graph(a)
+# nodelist = [n for n in G.neighbors(neuron)]
+# pos = {i:np.random.rand(2) for i in range(n)}
+# nodes = nx.draw_networkx_nodes(G, pos=pos, nodelist=nodelist, node_color="grey")
+# nodes.set_edgecolor("yellow")
+# nodes = nx.draw_networkx_nodes(G, pos=pos, nodelist=[neuron], node_color="blue")
+# edgelist = [i for i in G.edges() if (i[0]== neuron)]
+# nx.draw_networkx_edges(G, pos=pos, edgelist=edgelist)
 
-print("JOIN")
+
+print("*****CREATING VISUALIZATIONS FOR JOIN*****")
 net.create_item("B", disjoint=True)
 net.visualize_network(layout=layout, savename=savepath+"beforeJOIN")
 net.join('A', 'B', 'C')
@@ -33,7 +47,12 @@ net.visualize_network(layout=layout, savename=savepath+"afterJOIN_fireB")
 net.run_firing_step()
 net.visualize_network(layout=layout, savename=savepath+"afterJOIN_fireB_stepone")
 
-print("LINK")
+print("*****CREATING VISUALIZATIONS FOR LINK*****")
+n = 30
+d = 10
+k = 2
+r = 5
+layout={i:np.random.rand(2) for i in range(n)}
 net.reset_network(reset_synapse_strengths=True, reset_items=True)
 net.create_item("A", disjoint=True)
 net.create_item("B", disjoint=True)
--- Code/neuroidal_model_test_visualization.py
+++ Code/neuroidal_model_test_visualization.py
@@ -13,37 +13,47 @@ net.create_item("A")
 net.stored_items
 net.create_item("B")
 net.stored_items
-#
-# print("\nTest Firing")
-# net.fire_item("A")
-# net.neuron_firings
-# net.neuron_memories
-# print(net.get_firing_items())
-# net.fire_item("B")
-# net.neuron_firings
-# net.neuron_memories
-# net.visualize_network()
-# print(net.get_firing_items())
-# net.turn_off_all_firing()
-# net.neuron_firings
-# net.neuron_memories
-# print(net.get_firing_items())
-
 
 print("\nTest Joining")
 print("Joining item A and B")
 net.join('A', 'B', 'C')
-layout=net.visualize_network()
 print("Firing item A and B")
 net.fire_item("A")
 net.fire_item("B")
 net.visualize_network(layout=layout)
-print(layout)
-
 print("Firing items: ")
 print(net.get_firing_items())
 print("Running update step:")
 net.run_firing_step()
 print("Firing items: ")
 print(net.get_firing_items())
+net.visualize_network(layout=layout)
+
+
+net.reset_network(reset_synapse_strengths=True)
+print("\nTest Linking")
+net.turn_off_all_firing()
+net.fire_item('A')
+print('after firing A from scratch, firing items are: ', net.get_firing_items())
+net.run_firing_step()
+net.visualize_network(layout=layout)
+print('after firing A from scratch, waiting 1 step, firing items are: ', net.get_firing_items())
+net.visualize_network(layout=layout)
+net.turn_off_all_firing()
+net.fire_item('B')
+print('after firing B from scratch, firing items are: ', net.get_firing_items())
+net.visualize_network(layout=layout)
+net.run_firing_step()
+print('after firing B from scratch, waiting 1 step, firing items are: ', net.get_firing_items())
+net.visualize_network(layout=layout)
+net.turn_off_all_firing()
+net.link('A', 'B')
+net.fire_item('A')
+print('after linking A-B, firing A from scratch, firing items are: ', net.get_firing_items())
+net.visualize_network(layout=layout)
+net.run_firing_step()
+print('after linking A-B, firing A from scratch, waiting 1 step, firing items are: ', net.get_firing_items())
+net.visualize_network(layout=layout)
+net.run_firing_step()
+print('after linking A-B, firing A from scratch, waiting 2 steps, firing items are: ', net.get_firing_items())
 net.visualize_network(layout=layout)
